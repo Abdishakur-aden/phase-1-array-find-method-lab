260e05745d5b459939eb743c6e784217d918fe86
Abdishakur Mukhtar <abdishakurmukhtar46@gmail.com>
2024-03-28 14:02:58 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(kkk) {
+    const yyy = kkk.find ( kkk => kkk.result === "W")
+    if (yyy) {
+        return yyy.year;      
+    }else {
+        return undefined
+    }
+}
